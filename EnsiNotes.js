7e3174bb5e04bfd67ddd41cf7ef4d3c3cafdff85
Eduardo San Martin Morote <eduardo.san-martin-morote@ensimag.grenoble-inp.fr>
2014-06-20 21:19:34 +0200

    hook + namespace test

--- EnsiNotes.js
+++ EnsiNotes.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        EnsiNotes
-// @namespace   https://intranet.ensimag.fr/Zenith2/ConsultNotes?uid=*
+// @namespace   imag
 // @description Notes Zenith 2
 // @icon        https://raw.githubusercontent.com/posva/imag-utils/master/logo.png
 // @include     https://intranet.ensimag.fr/Zenith2/ConsultNotes?uid=*
